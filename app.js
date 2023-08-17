c234d20bfa56e3658a2b122bc4ce2eab4b90ca56
cristian <127469110+NarvaezSKY@users.noreply.github.com>
2023-08-16 22:22:00 -0500

    fotos omaga

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     let carrito = document.getElementById('carrito');
     let botonCompra = document.querySelectorAll('#comprar');
     let producto = document.querySelectorAll('.card');
-
+    let contenidotal=document.getElementById('carritoHover')
+    
     let total = 0;
     let carritoProductos = {};
-  
-    
+    carritoProductos=JSON.parse(localStorage.getItem("producto"))||{}
+    total=JSON.parse(localStorage.getItem("total"))
+
+    // console.log(carritoProductos)
+
 
     botonCompra.forEach((boton, index) => {
         boton.addEventListener('click', () => {
             const productoSeleccionado = producto[index];
             const imagenProducto = productoSeleccionado.querySelector('img').src;
+            console.log(imagenProducto)
             const precioProducto = parseInt(productoSeleccionado.querySelector('h5').textContent);
             const descProducto = productoSeleccionado.querySelector('.card-text').textContent;
 
@@ -21,47 +26,56 @@ document.addEventListener('DOMContentLoaded', () => {
                 carritoProductos[descProducto].cantidad++;
             } else {
                 carritoProductos[descProducto] = {
+                    imagen: imagenProducto,
                     precio: precioProducto,
                     cantidad: 1,
-                };
+                };  
             }
 
-            total += precioProducto;
-            console.log(total)
+            total+=precioProducto
+            
+            actualizarCarrito(carritoProductos, total)
+            subirLocal(carritoProductos, total)
+            
+        });
+    });
+
+        
             function actualizarCarrito() {
                 carrito.innerHTML = '';
-        
+
                 for (const producto in carritoProductos) {
+                    // console.log(carritoProductos)
                     const infoProducto = carritoProductos[producto];
-                    // console.log(infoProducto)
                     const elementoCarrito = document.createElement('div');
+                    
+                 console.log(infoProducto)
                     elementoCarrito.innerHTML = `
+                        <img src="${infoProducto.imagen}" class="foticosuwu">
                         <p class="valor">${infoProducto.precio}</p>
                         <p>${producto}</p>
-                        <p>Cantidad: ${infoProducto.cantidad}</p>
+                        <p>Cantidad: <b>${infoProducto.cantidad}</b></p>
                     `;
                     elementoCarrito.classList.add("productoEnCarro");
-                   
-                    carrito.appendChild(elementoCarrito);
 
-                    let carritoLocalStorage=JSON.stringify(carritoProductos)
-                    localStorage.setItem("producto",carritoLocalStorage)
-                    traerDelTal=localStorage.getItem("producto")
-                    traerElTal=JSON.parse(traerDelTal)
-                    console.log(traerElTal)
+                    carrito.appendChild(elementoCarrito);
+                    
+                    
                 }
-        
+              
+     
                 const totalElemento = document.createElement('div');
                 totalElemento.innerHTML = `<p class="totalProductos">Total: $${total}</p>`;
                 carrito.appendChild(totalElemento);
             }
-            actualizarCarrito();
-            console.log(carritoProductos)
-        });
-    });
+            
+            function subirLocal (carritoProductos){
+        
+                localStorage.setItem("producto", JSON.stringify(carritoProductos))
+                localStorage.setItem("total",JSON.stringify(total))
 
-
-
+              }
+            
 
     icono.addEventListener('mouseover', () => {
         carrito.style.display = "block";
@@ -73,6 +87,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-
-
-{/* <img class="foticosuwu" src="${imagenProducto}"> */}
